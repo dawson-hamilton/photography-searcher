58610ddb877eba163ab22291c459c7776bf611ad
PRISON.MIKE <dawsondhamilton@gmail.com>
2020-01-02 12:29:08 -0700

    getting the base started, also getting the time started on the front page

---
+++ script.js
@@ -0,0 +1,9 @@
+//put time in the beginning
+$("#time").text(moment().format('MMMM Do YYYY,HH:mm:ss'));
+
+//set the time on the top and update every second
+setInterval(function () {
+    $("#time").text(moment().format('MMMM Do YYYY,HH:mm:ss'));
+}, 1000);
+
+setInterval();
