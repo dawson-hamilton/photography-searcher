94ba76005c0c41dd9ad9ffe7322b3c3388b55a29
PRISON.MIKE <dawsondhamilton@gmail.com>
2020-01-04 12:34:26 -0700

    finishing up the js for the apod api call, making the css look good as well

--- nasa/nasa-script.js
+++ nasa/nasa-script.js
@@ -10,15 +10,21 @@ setInterval(function () {
 
 function buildApodUrl() {
     var queryUrl = "https://api.nasa.gov/planetary/apod?";
-    var apodParams = { "api-key": "aAa4zky4JtLr7ASY61ZetQhJmbconfw4g2rZW9RR&" };
+    var apodParams = { "api_key": "aAa4zky4JtLr7ASY61ZetQhJmbconfw4g2rZW9RR" };
     return queryUrl + $.param(apodParams);
 }
 
 var apodUrl = buildApodUrl();
+console.log(apodUrl);
 $.ajax({
-    URL: apodUrl, method: "GET"
+    url: apodUrl, method: "GET"
 }).then(function (data) {
+    console.log(data);
     var apodData = data;
 
     $(".title").text(apodData.title);
+    var imgEl = $("<img class'col s10'>").attr("src", apodData.url);
+    $(".apod-img").append(imgEl);
+    $(".explanation").text(apodData.explanation);
+    $(".date").text(apodData.date);
 });
