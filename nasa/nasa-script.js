34a76445d53f0d90e4be55486489598f17e5f32d
PRISON.MIKE <dawsondhamilton@gmail.com>
2020-01-04 10:50:05 -0700

    made the basic ajax call for the apod api

--- nasa/nasa-script.js
+++ nasa/nasa-script.js
@@ -8,3 +8,17 @@ setInterval(function () {
     $("#time").text(moment().format('MMMM Do YYYY,HH:mm:ss'));
 }, 1000);
 
+function buildApodUrl() {
+    var queryUrl = "https://api.nasa.gov/planetary/apod?";
+    var apodParams = { "api-key": "aAa4zky4JtLr7ASY61ZetQhJmbconfw4g2rZW9RR&" };
+    return queryUrl + $.param(apodParams);
+}
+
+var apodUrl = buildApodUrl();
+$.ajax({
+    URL: apodUrl, method: "GET"
+}).then(function (data) {
+    var apodData = data;
+
+    $(".title").text(apodData.title);
+});
